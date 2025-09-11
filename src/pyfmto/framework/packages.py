bd262ac6c936a43f0f5b72c171b8c233838a1276
Xiaoxu-Zhang <xxzhang_official@163.com>
2025-09-11 17:53:55 +0800

    feat(packages): add SyncDataManager class for managing synchronization data
    - Introduced `SyncDataManager` to handle source and result data synchronization.
    - Added methods to update, retrieve, and manage versions of the data.
    - Included properties to get the latest source version, result version, available source version, and number of clients.
    - Add unittests for SyncDataManager

--- src/pyfmto/framework/packages.py
+++ src/pyfmto/framework/packages.py
@@ -1,7 +1,8 @@
+from collections import defaultdict
 from enum import Enum, auto
 from typing import Optional, Any
 
-__all__ = ['Actions', 'ClientPackage', 'ServerPackage', 'DataArchive']
+__all__ = ['Actions', 'ClientPackage', 'ServerPackage', 'DataArchive', 'SyncDataManager']
 
 
 class Actions(Enum):
@@ -20,6 +21,48 @@ class ServerPackage:
     def __init__(self, desc: str, data=None):
         self.desc = desc
         self.data = data
+
+
+class SyncDataManager:
+    def __init__(self):
+        self._source: dict[int, dict[int, Any]] = defaultdict(dict)
+        self._result: dict[int, dict[int, Any]] = defaultdict(dict)
+
+    def update_src(self, cid: int, version: int, data: Any):
+        self._source[cid][version] = data
+
+    def update_res(self, cid: int, version: int, data: Any):
+        self._result[cid][version] = data
+
+    def lts_src_ver(self, cid: int) -> int:
+        return max(self._source.get(cid, {-1: None}).keys())
+
+    def lts_res_ver(self, cid: int) -> int:
+        return max(self._result.get(cid, {-1: None}).keys())
+
+    def get_src(self, cid: int, version: int):
+        try:
+            return self._source[cid][version]
+        except KeyError:
+            return None
+
+    def get_res(self, cid: int, version: int):
+        try:
+            return self._result[cid][version]
+        except KeyError:
+            return None
+
+    @property
+    def available_src_ver(self) -> int:
+        vers = [max(data.keys()) for data in self._source.values()]
+        if vers:
+            return min(vers)
+        else:
+            return -1
+
+    @property
+    def num_clients(self) -> int:
+        return len(self._source)
 
 
 class DataArchive:
