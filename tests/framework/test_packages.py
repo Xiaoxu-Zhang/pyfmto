c5692aa4eb25c728e5f7d894527f1503c3ada0f2
Xiaoxu Zhang <xxzhang_official@163.com>
2025-09-11 17:50:19 +0800

    test(test_packages): add unit tests for SyncDataManager

--- tests/framework/test_packages.py
+++ tests/framework/test_packages.py
@@ -1,13 +1,19 @@
+import shutil
 import unittest
+
 from pyfmto.framework.packages import (
     Actions,
     ClientPackage,
     ServerPackage,
-    DataArchive
+    DataArchive,
+    SyncDataManager
 )
 
 
 class TestPackagesForCoverage(unittest.TestCase):
+    def tearDown(self):
+        shutil.rmtree('out', ignore_errors=True)
+
     def test_actions_enum(self):
         self.assertEqual(Actions.REGISTER.name, 'REGISTER')
         self.assertEqual(Actions.QUIT.name, 'QUIT')
@@ -55,3 +61,56 @@ class TestPackagesForCoverage(unittest.TestCase):
         archive.add_res("res2")
         self.assertEqual(archive.num_res, 2)
         self.assertEqual(archive.get_latest_res(), "res2")
+
+
+class TestSyncDataManager(unittest.TestCase):
+
+    def setUp(self):
+        self.sync_manager = SyncDataManager()
+
+    def tearDown(self):
+        shutil.rmtree('out', ignore_errors=True)
+
+    def test_update_src(self):
+        self.sync_manager.update_src(1, 100, "test_data")
+        self.assertEqual(self.sync_manager.get_src(1, 100), "test_data")
+
+    def test_update_res(self):
+        self.sync_manager.update_res(1, 200, "result_data")
+        self.assertEqual(self.sync_manager.get_res(1, 200), "result_data")
+
+    def test_lts_src_ver(self):
+        self.sync_manager.update_src(1, 100, "test_data")
+        self.sync_manager.update_src(1, 101, "newer_data")
+        self.assertEqual(self.sync_manager.lts_src_ver(1), 101)
+
+    def test_lts_res_ver(self):
+        self.sync_manager.update_res(1, 200, "result_data")
+        self.sync_manager.update_res(1, 201, "newer_result")
+        self.assertEqual(self.sync_manager.lts_res_ver(1), 201)
+
+    def test_get_src(self):
+        self.sync_manager.update_src(1, 100, "test_data")
+        self.assertEqual(self.sync_manager.get_src(1, 100), "test_data")
+        self.assertIsNone(self.sync_manager.get_src(2, 100))
+
+    def test_get_res(self):
+        self.sync_manager.update_res(1, 200, "result_data")
+        self.assertEqual(self.sync_manager.get_res(1, 200), "result_data")
+        self.assertIsNone(self.sync_manager.get_res(2, 200))
+
+    def test_available_src_ver(self):
+        self.assertEqual(self.sync_manager.available_src_ver, -1)
+        self.sync_manager.update_src(1, 100, "data1")
+        self.sync_manager.update_src(1, 101, "data2")
+        self.sync_manager.update_src(2, 100, "data3")
+        self.sync_manager.update_src(2, 101, "data4")
+        self.sync_manager.update_src(2, 102, "data5")
+        self.assertEqual(self.sync_manager.available_src_ver, 101)
+
+    def test_num_clients(self):
+        self.assertEqual(self.sync_manager.num_clients, 0)
+        self.sync_manager.update_src(1, 100, "data1")
+        self.assertEqual(self.sync_manager.num_clients, 1)
+        self.sync_manager.update_src(2, 200, "data2")
+        self.assertEqual(self.sync_manager.num_clients, 2)
