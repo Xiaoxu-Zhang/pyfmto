fae54a6ad760df9409e38afcf306fd33270356ee
Xiaoxu Zhang <xxzhang_official@163.com>
2025-11-16 20:27:44 +0800

    docs(experiments): clarify image merge and alpha parameter comments

--- src/pyfmto/experiments/__init__.py
+++ src/pyfmto/experiments/__init__.py
@@ -25,14 +25,14 @@ curve:
     suffix: .png  # File extension for the generated curve image. options: '.png', '.jpg', '.eps', '.svg', '.pdf'
     quality: 3  # int in [1, 9]. Image quality parameter, affecting the quality of scalar images.
     showing_size: -1  # Use 'last showing_size data points' to plot curve. If -1, use all data points.
-    merge: True  # Merge all plotted images to a single figure or not. Note that only '.png' and '.jpg' files can be merged.
+    merge: True  # Merge all plotted images to a single figure or not. Only support for '.png' and '.jpg' files.
     clear: True  # If True, clear plots after merge. Only applicable when merge is True.
     on_log_scale: False  # The plot is generated on a logarithmic scale.
-    alpha: 0.2  # Transparency of the Standard Error region, ranging from 0 (completely transparent) to 1 (completely opaque).
+    alpha: 0.2  # Transparency of the Standard Error region, 0 to 1 (completely to opaque).
 violin:
     suffix: .png  # File extension for the generated violin image. Options: '.png', '.jpg', '.eps','.svg', '.pdf'.
     quality: 3   # int in [1, 9]. Image quality parameter, affecting the quality of scalar images.
-    merge: bool  # Merge all plotted images to a single figure or not. Note that only '.png' and '.jpg' files can be merged.
+    merge: bool  # Merge all plotted images to a single figure or not. Only support for '.png' and '.jpg' files.
     clear: True  # If True, clear plots after merge. Only applicable when merge is True.
 console:
     pvalue: 0.5 # T-test threshold for determining statistical significance.
