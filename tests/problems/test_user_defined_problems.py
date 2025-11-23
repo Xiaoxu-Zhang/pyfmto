b4d7ae97ea9f81def6ef99cf16cf4c1af64541e5
Xiaoxu Zhang <xxzhang_official@163.com>
2025-11-23 13:18:59 +0800

    test(core): refactor user defined problem tests
    Simplify test setup by removing complex problem class definitions and using helper generators. Replace manual file creation with gen_problem utility and clean up with remove_temp_files.

--- tests/problems/test_user_defined_problems.py
+++ tests/problems/test_user_defined_problems.py
@@ -1,51 +1,16 @@
-import shutil
-from pathlib import Path
 from unittest import TestCase
 
 from pyfmto import list_problems
-
-src_init = "from .my_prob import MyMTP"
-src_prob = """
-import numpy as np
-from typing import Union
-from numpy import ndarray
-from pyfmto.problems import SingleTaskProblem, MultiTaskProblem
-
-
-class MySTP(SingleTaskProblem):
-
-    def __init__(self, dim=2, **kwargs):
-        super().__init__(dim=dim, obj=1, lb=0, ub=1, **kwargs)
-
-    def _eval_single(self, x: ndarray):
-        return np.sum(x)
-
-
-class MyMTP(MultiTaskProblem):
-    is_realworld = False
-    intro = "user defined MTP"
-    notes = "for test purposes only"
-    references = ['no reference']
-
-    def __init__(self, dim=10, **kwargs):
-        super().__init__(dim, **kwargs)
-
-    def _init_tasks(self, dim, **kwargs) -> Union[list[SingleTaskProblem], tuple[SingleTaskProblem]]:
-        return [MySTP(dim=dim, **kwargs) for _ in range(10)]
-"""
+from tests.helpers.cleaners import remove_temp_files
+from tests.helpers.generators import gen_problem
 
 
 class TestUserDefinedProblems(TestCase):
     def setUp(self):
-        self.problems_dir = Path('problems')
-        self.problems_dir.mkdir(exist_ok=True)
-        with open(self.problems_dir / '__init__.py', 'w') as f:
-            f.write(src_init)
-        with open(self.problems_dir / 'my_prob.py', 'w') as f:
-            f.write(src_prob)
+        gen_problem('PROB1')
 
     def tearDown(self):
-        shutil.rmtree(self.problems_dir)
+        remove_temp_files()
 
     def test_load_user_defined_problem(self):
-        self.assertTrue('MyMTP' in list_problems())
+        self.assertTrue('PROB1' in list_problems())
