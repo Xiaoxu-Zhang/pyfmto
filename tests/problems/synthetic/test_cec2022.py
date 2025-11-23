a720b7eb3ca6a6209cc74cbce5a2668bf80f95a9
Xiaoxu Zhang <xxzhang_official@163.com>
2025-11-23 13:17:29 +0800

    test(core): update problem name references in tests

--- tests/problems/synthetic/test_cec2022.py
+++ tests/problems/synthetic/test_cec2022.py
@@ -6,8 +6,8 @@ from pyfmto import init_problem
 class TestCec2022(unittest.TestCase):
 
     def test_valid_dim(self):
-        init_problem('cec2022', dim=10)
-        init_problem('cec2022', dim=20)
+        init_problem('Cec2022', dim=10)
+        init_problem('Cec2022', dim=20)
 
     def test_invalid_dim(self):
-        self.assertRaises(ValueError, init_problem, name='cec2022', dim=9)
+        self.assertRaises(ValueError, init_problem, name='Cec2022', dim=9)
--- tests/problems/synthetic/test_tetci2019.py
+++ tests/problems/synthetic/test_tetci2019.py
@@ -6,9 +6,9 @@ from pyfmto import init_problem
 class TestTetci2019(unittest.TestCase):
 
     def test_high_dimensional(self):
-        prob = init_problem('tetci2019', dim=28)
+        prob = init_problem('Tetci2019', dim=28)
         self.assertEqual(prob.task_num, 8)
         _ = str(prob)
 
     def test_invalid_dim(self):
-        self.assertRaises(ValueError, init_problem, name='tetci2019', dim=51)
+        self.assertRaises(ValueError, init_problem, name='Tetci2019', dim=51)
