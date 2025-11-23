184ea5af61f82f229b051d439ff5f317e9789475
Xiaoxu Zhang <xxzhang_official@163.com>
2025-11-23 12:12:49 +0800

    refactor(core): update return type annotation for __getitem__

--- src/pyfmto/problems/problem.py
+++ src/pyfmto/problems/problem.py
@@ -712,7 +712,7 @@ class MultiTaskProblem(ABC):
     def __iter__(self):
         return iter(self._problem)
 
-    def __getitem__(self, index) -> SingleTaskProblem:
+    def __getitem__(self, index) -> Union[SingleTaskProblem, list[SingleTaskProblem]]:
         if isinstance(index, slice):
             return self._problem[index]
         elif isinstance(index, int):
