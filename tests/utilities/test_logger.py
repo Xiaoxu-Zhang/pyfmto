279c09d68564c9dd9afe6f03f77b93a4523ecab5
Xiaoxu Zhang <xxzhang_official@163.com>
2025-12-02 11:18:07 +0800

    test(logger): refactor logger tests to use custom handler

--- tests/utilities/test_logger.py
+++ tests/utilities/test_logger.py
@@ -1,35 +1,30 @@
-import unittest
+import logging.config
+from unittest.mock import patch
 
-from pyfmto.utilities import loggers, logger, reset_log
+from pyfmto.utilities.loggers import LOG_CONF, PyfmtoRotatingFileHandler
+from pathlib import Path
+from unittest import TestCase
+
 from tests.helpers import remove_temp_files
 
 
-class TestLoggers(unittest.TestCase):
+class TestPyfmtoRotatingFileHandler(TestCase):
+    def setUp(self):
+        self.log_dir = Path("out/logs")
+        self.log_dir.mkdir(exist_ok=True, parents=True)
+
+        # Configure logging
+        logging.config.dictConfig(LOG_CONF)
+        self.logger = logging.getLogger('pyfmto')
 
     def tearDown(self):
         remove_temp_files()
 
-    def test_init_conf_loads_config_correctly(self):
-        self.assertIsNotNone(logger)
-        self.assertTrue(logger.hasHandlers())
-
-    def test_reset_log_files_backs_up_and_resets_all(self):
-        loggers.reset_log()
-        with open(loggers.LOG_FILE, 'w', encoding='utf-8') as f:
-            f.write("Original Content")
-
-        loggers.reset_log()
-
-        self.assertTrue(loggers.LOG_BACKUP.exists())
-        with open(loggers.LOG_BACKUP, 'r', encoding='utf-8') as f:
-            self.assertEqual(f.read(), "Original Content")
-        remove_temp_files()
-
-    def test_backup_to(self):
-        loggers.LOG_PATH.mkdir(parents=True, exist_ok=True)
-        with open(loggers.LOG_FILE, 'w', encoding='utf-8') as f:
-            f.write("Original Content")
-        reset_log()
-        backup_to = loggers.LOG_PATH / 'backup'
-        loggers.backup_log_to(backup_to)
-        self.assertTrue((backup_to / 'backup.log').exists())
+    @patch("time.strftime", return_value="2023-10-10 12:00:00")
+    def test_rotation_filename(self, mock_strftime):
+        handler = self.logger.handlers[0]
+        self.assertIsInstance(handler, PyfmtoRotatingFileHandler)
+        default_name = str("pyfmto.log")
+        rotated_name = handler.rotation_filename(default_name)
+        expected_name = str(self.log_dir / "pyfmto 2023-10-10 12:00:00.log")
+        self.assertEqual(rotated_name[-len(expected_name):], expected_name)
