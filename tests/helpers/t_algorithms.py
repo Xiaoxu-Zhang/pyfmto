427268812bedf432481df57a371274f93fb7230f
Xiaoxu Zhang <xxzhang_official@163.com>
2026-01-12 15:00:29 +0800

    fix(t_algorithms): add sync request and client join tracking in test helpers
    - Add request_server call with repeat 100 in optimize method
    - Implement check_pkg method to validate package presence
    - Add all_joined flag to ClassPrefixServer to track client joins
    - Update aggregate method to set all_joined when 4 clients join
    - Modify handle_request to return 'OK' only if all clients joined

--- tests/helpers/t_algorithms.py
+++ tests/helpers/t_algorithms.py
@@ -11,11 +11,14 @@ class ClassPrefixClient(Client):
     def __init__(self, problem, **kwargs):
         super().__init__(problem)
         self.problem.auto_update_solutions = True
-        self.conn_retry = 100
 
     def optimize(self):
         x = self.problem.random_uniform_x(1)
         self.problem.evaluate(x)
+        self.request_server(package=ClientPackage(self.id, 'sync'), repeat=100)
+
+    def check_pkg(self, pkg) -> bool:
+        return pkg is not None
 
 
 class ClassPrefixServer(Server):
@@ -25,9 +28,14 @@ class ClassPrefixServer(Server):
 
     def __init__(self, **kwargs):
         super().__init__()
+        self.all_joined = False
 
     def aggregate(self):
-        pass
+        if self.num_clients == 4:
+            self.all_joined = True
 
     def handle_request(self, pkg: ClientPackage) -> Any:
-        pass
+        if self.all_joined:
+            return 'OK'
+        else:
+            return None
