c9fba8f2afe19e35c7ae0c90f50c53b59ec03045
Xiaoxu Zhang <xxzhang_official@163.com>
2026-01-19 17:35:53 +0800

    test(algorithm): add unit tests for AlgorithmData class
    - Verify default parameter values and name attributes of AlgorithmData
    - Test updating of parameters and parameter differences tracking
    - Ensure params_yaml reflects correct content when no parameters exist
    - Confirm setting and retrieving of name alias attribute

---
+++ tests/framework/test_algorithm_data.py
@@ -0,0 +1,31 @@
+from pyfmto.framework import AlgorithmData
+from tests.utilities import LoadersTestCase
+
+
+class TestAlgorithmData(LoadersTestCase):
+
+    def test_default_value(self):
+        alg = self.load_algorithm('ALG1')
+        self.assertEqual(alg.params_default, {'client': {'name': 'c'}, 'server': {'name': 's'}})
+        self.assertEqual(alg.params_default, alg.params)
+        self.assertEqual(alg.name, 'ALG1')
+        self.assertEqual(alg.name_alias, '')
+        self.assertTrue('client' in alg.params_yaml)
+        self.assertNotEqual(alg.params_snapshot, '')
+
+    def test_update_params(self):
+        alg = self.load_algorithm('ALG1')
+        para_dft = {'client': {'name': 'c'}}
+        para_upd = {'client': {'name': 'cc', 'c_new': 'n'}, 'server': {'name': 'ss', 's_new': 'n'}}
+        alg.params_default = para_dft
+        self.assertEqual(alg.params, para_dft)
+        self.assertEqual(alg.params_diff, '')
+        alg.params_update = para_upd
+        self.assertEqual(alg.params, alg.params_update)
+        self.assertTrue(alg.params_diff != '')
+        alg.params_default = {}
+        alg.params_update = {}
+        self.assertIn('no configurable parameters', alg.params_yaml)
+        alg.name_alias = 'ALGG'
+        self.assertEqual(alg.name_alias, 'ALGG')
+        self.assertEqual(alg.name, 'ALGG')
