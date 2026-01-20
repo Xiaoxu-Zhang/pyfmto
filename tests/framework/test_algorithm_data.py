5a42c0623792aa90d21ea4cf5d37bb26b1731d9a
Xiaoxu Zhang <xxzhang_official@163.com>
2026-01-20 16:55:58 +0800

    test(algorithm): update test case to use generic algorithm loader
    - Replace LoadersTestCase with TestCaseAlgProbConf as base test class
    - Use dynamic algorithm name from alg_names list instead of hardcoded 'ALG1'
    - Adjust assertions to use dynamic algorithm name variable
    - Update test logic to improve flexibility and reuse in test_algorithm_data.py

--- tests/framework/test_algorithm_data.py
+++ tests/framework/test_algorithm_data.py
@@ -1,19 +1,21 @@
-from tests.utilities import LoadersTestCase
+from tests.helpers.testcases import TestCaseAlgProbConf
 
 
-class TestAlgorithmData(LoadersTestCase):
+class TestAlgorithmData(TestCaseAlgProbConf):
 
     def test_default_value(self):
-        alg = self.load_algorithm('ALG1')
+        alg_name = self.alg_names[0]
+        alg = self.load_algorithm(alg_name)
         self.assertEqual(alg.params_default, {'client': {'name': 'c'}, 'server': {'name': 's'}})
         self.assertEqual(alg.params_default, alg.params)
-        self.assertEqual(alg.name, 'ALG1')
+        self.assertEqual(alg.name, alg_name)
         self.assertEqual(alg.name_alias, '')
         self.assertTrue('client' in alg.params_yaml)
         self.assertNotEqual(alg.params_snapshot, '')
 
     def test_update_params(self):
-        alg = self.load_algorithm('ALG1')
+        alg_name = self.alg_names[0]
+        alg = self.load_algorithm(alg_name)
         para_dft = {'client': {'name': 'c'}}
         para_upd = {'client': {'name': 'cc', 'c_new': 'n'}, 'server': {'name': 'ss', 's_new': 'n'}}
         alg.params_default = para_dft
