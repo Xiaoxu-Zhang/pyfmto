408a509614ca439bbf0ec425ef6f14ea2ba5f0da
Xiaoxu Zhang <xxzhang_official@163.com>
2026-01-20 16:57:38 +0800

    test(framework): refactor tests to use updated problem loading and base test class
    - Replace multiple problem loadings with a single loaded and initialized problem
    - Change test class to inherit from updated TestCaseAlgProbConf base class
    - Simplify client creation by iterating over single problem instance
    - Remove unused imports and redundant setup/teardown methods
    - Adjust server and client test setups to align with new problem instance management
    - Maintain coverage for offline and online client behaviors and error handling

--- tests/framework/test_running.py
+++ tests/framework/test_running.py
@@ -3,25 +3,20 @@ import time
 
 from requests.exceptions import ConnectionError
 
-from pyfmto import list_problems, load_problem
+from pyfmto import load_problem
 from pyfmto.framework import Client, ClientPackage, Server
 from tests.framework import OfflineServer, OnlineClient, OnlineServer
-from tests.helpers import PyfmtoTestCase, gen_code, running_server, start_clients
+from tests.helpers import running_server, start_clients
+from tests.helpers.testcases import TestCaseAlgProbConf
 
 N_CLIENTS = 2
 
 
-class TestClientSide(PyfmtoTestCase):
+class TestClientSide(TestCaseAlgProbConf):
 
     def setUp(self):
-        self.save_sys_env()
-        gen_code('problems', 'PROB', self.tmp_dir)
-        list_problems(self.sources)
-        self.problems = load_problem('PROB', self.sources, dim=5, fe_init=20, fe_max=30).initialize()
-
-    def tearDown(self):
-        self.restore_sys_env()
-        self.delete(self.tmp_dir)
+        super().setUp()
+        self.problem = self.load_problem(self.prob_names[0], dim=5, fe_init=20, fe_max=30).initialize()
 
     def test_valid_offline_client(self):
         """An offline client doesn't request the server"""
@@ -31,16 +26,16 @@ class TestClientSide(PyfmtoTestCase):
                 x = self.problem.random_uniform_x(1)
                 y = self.problem.evaluate(x)
                 self.problem.solutions.append(x, y)
-        clients = [OfflineClient(prob) for prob in self.problems[:N_CLIENTS]]
+        clients = [OfflineClient(prob) for prob in self.problem[:N_CLIENTS]]
         with running_server(OfflineServer):
             start_clients(clients)
 
-        clients = [OnlineClient(prob) for prob in self.problems[:N_CLIENTS]]
+        clients = [OnlineClient(prob) for prob in self.problem[:N_CLIENTS]]
         with running_server(OnlineServer):
             start_clients(clients)
 
     def test_request_failed(self):
-        client = OnlineClient(self.problems[0])
+        client = OnlineClient(self.problem[0])
         with self.assertRaises(ConnectionError):
             start_clients([client])
 
@@ -54,20 +49,20 @@ class TestClientSide(PyfmtoTestCase):
             def optimize(self):
                 self.request_server(None)
 
-        clients = [ClientWithOptimizeErr(prob) for prob in self.problems[:N_CLIENTS]]
+        clients = [ClientWithOptimizeErr(prob) for prob in self.problem[:N_CLIENTS]]
         with running_server(OnlineServer):
             with self.assertRaises(RuntimeError):
                 start_clients(clients)
 
-        clients = [ClientWithRequestErr(prob) for prob in self.problems[:N_CLIENTS]]
+        clients = [ClientWithRequestErr(prob) for prob in self.problem[:N_CLIENTS]]
         with running_server(OnlineServer):
             with self.assertRaises(ValueError):
                 start_clients(clients)
 
     def test_valid_server(self):
         server = OnlineServer()
-        problems = load_problem('PROB', self.sources, dim=5, fe_init=20, fe_max=30).initialize()
-        clients = [OnlineClient(prob) for prob in problems[:N_CLIENTS]]
+        problem = load_problem(self.prob_names[0], self.sources, dim=5, fe_init=20, fe_max=30).initialize()
+        clients = [OnlineClient(prob) for prob in problem[:N_CLIENTS]]
         thread = threading.Thread(target=server.start)
         thread.start()
         time.sleep(1)
